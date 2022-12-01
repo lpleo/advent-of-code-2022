a327e13849a1c9f4c9d14a15f5cc13a5075c84ef
Leonardo <leli.cons@bene.it>
2022-12-01 12:31:58 +0100

    day 1-1 and 1-2 solution

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,32 @@
 fun main() {
+
+    fun getMaxCaloriesList(input: List<String>): MutableList<Int> {
+        val maxCaloriesList: MutableList<Int> = mutableListOf(0);
+        input.forEach {
+            if (it.isNotEmpty()) {
+                maxCaloriesList[maxCaloriesList.lastIndex] = maxCaloriesList[maxCaloriesList.lastIndex] + Integer.parseInt(it);
+            } else {
+                maxCaloriesList.add(0);
+            }
+        }
+        return maxCaloriesList
+    }
+
     fun part1(input: List<String>): Int {
-        return input.size
+        val maxCaloriesList: MutableList<Int> = getMaxCaloriesList(input)
+        return maxCaloriesList.max().or(0)
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val maxCaloriesList = getMaxCaloriesList(input)
+        val maxCaloriesOrderedList = maxCaloriesList.sortedDescending()
+        return maxCaloriesOrderedList[0] + maxCaloriesOrderedList[1] + maxCaloriesOrderedList[2]
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 8)
+    check(part2(testInput) == 17)
 
     val input = readInput("Day01")
     println(part1(input))
